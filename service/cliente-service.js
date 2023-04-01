30a28987b117899fdc6c4859002eb91c0c6c261a
GustavoBryan <devgustavob@gmail.com>
2023-04-01 17:13:07 -0300

    fetch

--- service/cliente-service.js
+++ service/cliente-service.js
@@ -18,26 +18,15 @@ const criarNovaLinha = (nome, email) => {
 const tabela = document.querySelector('[data-tabela]')
 
 const listaClientes = () => {
-    const promise = new Promise((resolve, reject) => {
-        const http = new XMLHttpRequest()
-        http.open('GET', 'http://localhost:3000/profile')
-
-        http.onload = () => {
-            if (http.status >= 400) {
-                reject(JSON.parse(http.response))
-            } else {
-                resolve(JSON.parse(http.response))
-            }
-        }
-        http.send()
-    });
-    return promise
+    return fetch(`http://localhost:3000/profile`)
+        .then(resposta => {
+            return resposta.json()
+        })
 }
 
-
 listaClientes()
-.then( data => {
-    data.forEach(elemento => {
-        tabela.appendChild(criarNovaLinha(elemento.nome, elemento.email))
-    })
-});
+    .then(data => {
+        data.forEach(elemento => {
+            tabela.appendChild(criarNovaLinha(elemento.nome, elemento.email))
+        })
+    });
