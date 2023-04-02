ec027e352e80fcbb79d426872aecc7982c76891e
GustavoBryan <devgustavob@gmail.com>
2023-04-02 15:17:26 -0300

    remove

--- admin/service/cliente-service.js
+++ admin/service/cliente-service.js
@@ -22,12 +22,13 @@ const criaCliente = (nome, email) => {
 }
 
 const removeCliente = () => {
-    fetch(`http://localhost:3000/profile`, {
+    fetch(`http://localhost:3000/profile/${id}`, {
         method: 'DELETE'
     })
 }
 
 export const clienteService = {
     listaClientes,
-    criaCliente
+    criaCliente, 
+    removeCliente
 }
